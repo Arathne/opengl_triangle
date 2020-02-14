aaae968c02fe11cb652c82f7f7b851001c6649a0
edtorrez <etorrez2017@gmail.com>
2020-02-14 09:21:59 -0500

    different vertices but similar result : ortho

---
+++ ortho/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+
+void draw( void )
+{
+    glClear( GL_COLOR_BUFFER_BIT );
+    
+    glBegin( GL_TRIANGLES );
+    
+    glVertex3f( -75, -75, 0 );
+    glVertex3f( 75, -75, 0 );
+    glVertex3f( 0, 75, 0 );
+    
+    glEnd();
+    
+    glFlush();
+}
+
+void resize( int width, int height )
+{
+    glOrtho( -100, 100, -100, 100, -1, 1 );
+}
+
+int main( int argc, char** argv )
+{
+    glutInit( &argc, argv );
+    
+    glutInitContextVersion( 4, 3 );
+    glutInitContextProfile( GLUT_COMPATIBILITY_PROFILE );
+
+    glutInitDisplayMode( GLUT_SINGLE | GLUT_RGBA );
+    glutInitWindowPosition( 300, 300 );
+    glutInitWindowSize( 500, 500 );
+    
+    glutCreateWindow( "viewport triangle" );
+    glutDisplayFunc( draw );
+    glutReshapeFunc( resize );
+    
+    glClearColor( 0.5, 0.5, 0.5, 1.0 );
+    
+    glutMainLoop();
+
+    return 0;
+}
---
+++ ortho/readme.md
@@ -0,0 +1,1 @@
+### notice how the vertices of the triangle are different because of glOrtho
