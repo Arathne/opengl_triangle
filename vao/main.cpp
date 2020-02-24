5ecdd85dab980f70885cc112ceab71a51989daeb
edtorrez <etorrez2017@gmail.com>
2020-02-24 07:00:59 -0500

    should render now : vao

--- vao/main.cpp
+++ vao/main.cpp
@@ -24,9 +24,10 @@ void draw (void)
     glClear( GL_COLOR_BUFFER_BIT );
     
     glBindVertexArray( vao[i] );
+    glEnableVertexAttribArray(0);
     
     glDrawArrays( GL_TRIANGLES, 0, 3 );
-
+    
     glFlush();
 }
 
@@ -48,6 +49,8 @@ void setup (void)
     glBindBuffer( GL_ARRAY_BUFFER, vbo[1] );
     glBufferData( GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW );
     glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*) 0 );
+
+    
 }
 
 void keyboard (unsigned char key, int x, int y)
