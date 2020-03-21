af89e0f5dcbb240fc8ee2f6d79a9f7fb691648d0
edtorrez <etorrez2017@gmail.com>
2020-03-21 19:26:38 -0400

    added animation : matrix-1

--- matrix-1/main.cpp
+++ matrix-1/main.cpp
@@ -33,12 +33,19 @@ void draw (void)
 
 void matrix (void)
 {
+    float time = glutGet(GLUT_ELAPSED_TIME)/1000.0f;
     glm::mat4 transform = glm::mat4(1.0f);
     transform = glm::scale( transform, glm::vec3(0.5, 0.5, 0.5) );   
-    transform = glm::rotate( transform, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f) );
+    transform = glm::rotate( transform, time, glm::vec3(0.0f, 0.0f, 1.0f) );
 
     unsigned int uniformLocation = glGetUniformLocation( shaderProgram, "transform" );
     glUniformMatrix4fv( uniformLocation, 1, GL_FALSE, glm::value_ptr(transform) );
+}
+
+void idle (void)
+{
+    matrix();
+    glutPostRedisplay();
 }
 
 void shaders (void)
@@ -105,6 +112,7 @@ int main( int argc, char** argv )
     glutCreateWindow( "shader 1" );
     glutDisplayFunc( draw );
     glutKeyboardFunc( keyboard );
+    glutIdleFunc( idle );
 
     glewInit();
     setup();
